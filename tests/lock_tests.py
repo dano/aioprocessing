93df9233aa3ce93a7385ad1b44f060c2328bc6b8
dano <oreilldf@gmail.com>
2014-08-02 19:20:12 -0400

    Fixing bugs

--- tests/lock_tests.py
+++ tests/lock_tests.py
@@ -4,9 +4,29 @@ import unittest
 import aioprocessing
 from multiprocessing import Process, Event
 
+def get_value(self):
+    try:
+        return self.get_value()
+    except AttributeError:
+        try:
+            return self._Semaphore__value
+        except AttributeError:
+            try:
+                return self._value
+            except AttributeError:
+                raise NotImplementedError
+
 class BaseTest(unittest.TestCase):
     def setUp(self):
         self.loop = asyncio.get_event_loop()
+
+    def assertReturnsIfImplemented(self, value, func, *args):
+        try:
+            res = func(*args)
+        except NotImplementedError:
+            pass
+        else:
+            return self.assertEqual(value, res)
 
 def do_lock_acquire(lock, e):
     lock.acquire()
@@ -48,9 +68,37 @@ class LockTest(BaseTest):
         self.loop.run_until_complete(do_async_lock())
 
 
-
 class RLockTest(LockTest):
     def setUp(self):
         super().setUp()
         self.lock = aioprocessing.AioRLock()
 
+class SemaphoreTest(BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.sem = aioprocessing.AioSemaphore(2)
+
+    def _test_semaphore(self, sem):
+        self.assertReturnsIfImplemented(2, get_value, sem)
+        self.assertEqual(sem.acquire(), True)
+        self.assertReturnsIfImplemented(1, get_value, sem)
+
+        @asyncio.coroutine
+        def sem_acquire():
+            self.assertEqual((yield from sem.coro_acquire()), True)
+        self.loop.run_until_complete(sem_acquire())
+        self.assertReturnsIfImplemented(0, get_value, sem)
+        self.assertEqual(sem.acquire(False), False)
+        self.assertReturnsIfImplemented(0, get_value, sem)
+        self.assertEqual(sem.release(), None)
+        self.assertReturnsIfImplemented(1, get_value, sem)
+        self.assertEqual(sem.release(), None)
+        self.assertReturnsIfImplemented(2, get_value, sem)
+
+    def test_semaphore(self):
+        sem = self.sem
+        self._test_semaphore(sem)
+        self.assertEqual(sem.release(), None)
+        self.assertReturnsIfImplemented(3, get_value, sem)
+        self.assertEqual(sem.release(), None)
+        self.assertReturnsIfImplemented(4, get_value, sem)
