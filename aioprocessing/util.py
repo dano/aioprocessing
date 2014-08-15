93ef43dfe8bd1324f1b726b68c9d26994ef0380d
dano <oreilldf@gmail.com>
2014-08-15 01:08:18 -0400

    Don't wrap results in futures.

--- aioprocessing/util.py
+++ aioprocessing/util.py
@@ -3,10 +3,10 @@ import asyncio
 def run_in_executor(executor, callback, *args, **kwargs):
     loop = asyncio.get_event_loop()
     if kwargs:
-        return asyncio.async(loop.run_in_executor(executor,
-                                    lambda: callback(*args, **kwargs)))
+        return loop.run_in_executor(executor,
+                                    lambda: callback(*args, **kwargs))
     else:
-        return asyncio.async(loop.run_in_executor(executor, callback, *args))
+        return loop.run_in_executor(executor, callback, *args)
 
 
 #def retype_instance(recvinst, sendtype, metaklass=type):
