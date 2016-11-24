09abdf488d9e9b967302d7f7be7a85e0b1c236e2
Dan O'Reilly <oreilldf@gmail.com>
2016-11-24 00:29:12 -0500

    Increment to version 0.0.2
    Signed-off-by: Dan O'Reilly <oreilldf@gmail.com>

--- aioprocessing/__init__.py
+++ aioprocessing/__init__.py
@@ -14,8 +14,8 @@ __all__ = ['AioProcess', 'AioManager', 'AioPipe', 'AioQueue', 'AioSimpleQueue',
 # is zero for an official release, positive for a development branch,
 # or negative for a release candidate or beta (after the base version
 # number has been incremented)
-version = "0.0.2dev2"
-version_info = (0, 0, 2, -101)
+version = "0.0.2"
+version_info = (0, 0, 2, 0)
 
 if hasattr(multiprocessing, 'get_context'):
     def _get_context():
